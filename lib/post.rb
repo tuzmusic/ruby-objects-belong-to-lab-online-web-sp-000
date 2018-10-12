496cf5f3b50f6d2e8d1c689145733ea74d75a9d7
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-12 18:19:23 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,14 @@
 class Post
   attr_accessor :title#, :author
 
-  def title
-    @title
-  end
-
   def author_name
     @author.name
   end
+
   def author
     @author
   end
+  
   def author=(author)
     @author = author
   end
